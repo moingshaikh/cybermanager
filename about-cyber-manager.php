5b4e06e5440b8c5a4690f4afd9bab1a5f571596d
Moin Shaikh <moinshaikh@users.noreply.github.com>
2016-07-19 20:59:53 +0530

    Add files via upload

---
+++ about-cyber-manager.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/template.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<!--
+/*
+* Cyber Manager (v1.0) - Free For All Cyber Cafe Solution
+* http://www.moinshaikh.wordpress.com
+* Moin Shaikh
+* Version 1.0 (20-03-2011)
+* 
+* Copyright (c) 2011 Moin Shaikh
+* Permission is hereby granted, free of charge, to any person
+* obtaining a copy of this software and associated documentation
+* files (the "Software"), to deal in the Software without
+* restriction, including without limitation the rights to use, copy,
+* modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+* BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+* ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
+ -->
+ <head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>About Cyber Manager(v1.0)</title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" --><!-- InstanceEndEditable -->
+<script type="text/javascript" language="javascript" src="js/validation.js"></script>
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
+<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+</head>
+
+<body class="body">
+<table width="1000" align="center" height="auto" border="1" cellspacing="2" cellpadding="2">
+  <tr>
+    <td colspan="2" align="center"><table border="0" cellpadding="0" cellspacing="0" width="100%" height="auto" align="center">
+				<tr>
+					<td width="22%" align="left" valign="middle"><a href="cpanel.php" title="Go to Home Page" ><img src="images/home.png" align="middle" border="0" width="56" height="56" /></a></td>
+					<td width="50%" align="center"><img src="images/logo_new.gif" width="400" height="150" title="Cyber Manager(v1.0)" /></td>
+					<td width="14%" align="center" valign="middle" style="padding:5px"><a href="logout.php" title="Logout!"><img src="images/logout.gif" width="85" height="85" border="0" /></a></td>
+				<tr>
+					<td colspan="3" align="right">
+
+<span id="liveclock"></span></td>
+				</tr>				
+			</table></td>
+  </tr>
+  <tr>
+    <td width="25%" valign="top">
+
+<span class="arrowlistmenu">
+<a href="modules/customer/search-customer.php" style="text-decoration:none;"><h3 class="menuheader"><img src="images/customer.gif" border="0" width="32" height="32" align="absmiddle" style="padding-right:10px;"/>CUSTOMER</h3></a>
+<a href="modules/customer-login/customer-login.php" style="text-decoration:none;"><h3 class="menuheader"><img src="images/cust_login.png" border="0" width="32" height="32" align="absmiddle" style="padding-right:10px;"/>CUSTOMER LOG-IN</h3></a>
+<a href="modules/bills/index.php" style="text-decoration:none;"><h3 class="menuheader"><img src="images/bill.gif" border="0" width="32" height="32" align="absmiddle" style="padding-right:10px;"/>BILLS</h3></a>
+<a href="modules/services/delete-services.php" style="text-decoration:none;"><h3 class="menuheader"><img src="images/services.gif" border="0" width="32" height="32" align="absmiddle" style="padding-right:10px;"/>SERVICES</h3></a>
+<a href="modules/rate-card/print-rate-card.php" style="text-decoration:none;"><h3 class="menuheader"><img src="images/rate.gif" border="0" width="32" height="32" align="absmiddle" style="padding-right:10px;"/>RATE CARD</h3></a>
+<a href="modules/customer-card/index.php" style="text-decoration:none;"><h3 class="menuheader"><img src="images/membership-card.gif" border="0" width="32" height="32" align="absmiddle" style="padding-right:10px;"/>CUSTOMER CARD</h3>
+</a>
+<a href="modules/expired-id-report/expirydate.php" style="text-decoration:none;"><h3 class="menuheader"><img src="images/expired-id-proof.gif" border="0" width="32" height="32" align="absmiddle" style="padding-right:10px;"/>Expired ID Proofs </h3>
+</a>
+</span>    </td>
+    <td width="82%"><!-- InstanceBeginEditable name="body" -->
+	
+		<table border="0" align="center" width="100%" cellpadding="0" cellspacing="0">
+			<tr>
+				<td><h3 class="form-text-titles">About Cyber Manager (v1.0)</h3></td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td class="about-page-text">Hello and welcome to Cyber Manager(v1.0). This page provides you a brief introductory details about Cyber Manager(v1.0). Cyber Manager(v1.0) is a web based application that can be used either on your website using your web host account or it can be run and used by installing on your computer hard drive's localhost.</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+			  <td class="about-page-text">Cyber Manager(v1.0) is a reliable, efficient and powerfull solution to the problems that are being faced by Cyber Cafe administrators who are managing cyber cafe manually. And the Great thing about this Cyber Manager(v1.0) is that its an Open Source software (Free for all!). This application is distributed under <a href="gnu-gpl-license.html" title="GNU GPL License" target="_blank" class="link"><em><strong>GNU General Public Liecense</strong></em></a>. <br /></td>
+			</tr>
+			<tr>
+				<td>
+					<h4 class="form-text-titles">Salient features of Cyber Manager(v1.0)</h4>
+					<ul type="square" class="about-page-text">
+						<li>Open Source. Free for all!</li>
+						<li>Web Application, runs online / offline.</li>
+						<li>Easy to Install. </li>
+						<li>Easy to Use. </li>
+						<li>Strong security mechanisms. </li>
+						<li>User Friendly Interface. </li>
+						<li>Easily available. (via many open source software directories.) </li>
+						<li>Can be customized by users as per their specific needs.</li>
+						<li>Cross Browser Support.</li>
+						<li>Basic FAQs available for trouble shoot.</li>
+						<li>Updates will be released time by time.</li>
+					</ul>
+				</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+		</table>
+	<!-- InstanceEndEditable --></td>
+  </tr>
+  <tr>
+    <td colspan="2" align="center">
+		<table border="0" align="center" width="100%" cellpadding="0" cellspacing="0">
+			<tr>
+				<td align="center"><a href="cpanel.php" title="Go to cPanle Home"><img src="images/home.png" width="100" height="100" border="0" /></a></td>
+				<td align="center">|</td>
+				<td align="center"><a href="about-cyber-manager.php" title="About Cyber Manager(v1.0)"><img src="images/about-us.gif" width="100" height="100" border="0" /></a></td>
+				<td align="center">|</td>
+				<td align="center"><a href="user-manual-cybermanager(v1.0).pdf" target="_blank" title="User Manual - User Guide"><img src="images/user-manual.gif" alt="Help" height="100" width="100" border="0" /></a></td>
+				<td align="center">|</td>
+				<td align="center"><a href="http://www.moinshaikh.wordpress.com" target="_blank" title="Contact Developer"><img src="images/contact-icon.gif" width="100" height="100" border="0" /></a></td>
+			</tr>
+		</table>
+	</td>
+  </tr>
+</table>
+</body>
+<!-- InstanceEnd --></html>
