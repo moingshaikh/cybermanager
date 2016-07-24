691cb1ce8b72c392082c81c23b81ab5780d75bf7
Moin Shaikh <moinshaikh@users.noreply.github.com>
2016-07-24 09:45:08 -0400

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -1,5 +1,5 @@
 <!--
-*CHECKLOGIN.PHP - This will check for the login status before the admin/user can use this application.*
+*CHECKLOGIN.PHP - This will check for the login condition before the admin/user can use this application.*
 /*
 * Cyber Manager (v1.0) - Free For All Cyber Cafe Solution
 * http://www.moinshaikh.wordpress.com
