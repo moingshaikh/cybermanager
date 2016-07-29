a0635f6091e197bc84e3a8d64dbab16b27a0c3ed
Moin Shaikh <moinshaikh@users.noreply.github.com>
2016-07-29 13:17:30 -0400

    Update validation.js

--- js/validation.js
+++ js/validation.js
@@ -11,68 +11,68 @@ function ValidateAddCustForm()
         valid = false;
     }
 
-	    else if ( document.add_customer.l_name.value == "" )
+	    else if (document.add_customer.l_name.value == "" )
     {
         alert ( "Please enter your 'Last Name'." );
 		add_customer.l_name.focus();
         valid = false;
     }
 
-	    else if ( document.add_customer.street.value == "" )
+	    else if (document.add_customer.street.value == "" )
     {
         alert ( "Please enter your 'Street Address'." );
 		add_customer.street.focus();
         valid = false;
     }
-	    else if ( document.add_customer.city.value == "" )
+	    else if (document.add_customer.city.value == "" )
     {
         alert ( "Please enter your 'City's Name'." );
 		add_customer.city.focus();
         valid = false;
     }
-	    else if ( document.add_customer.pincode.value == "" )
+	    else if (document.add_customer.pincode.value == "" )
     {
         alert ( "Please enter your 'Pincode Number'." );
 		add_customer.pincode.focus();
         valid = false;
     }
-	    else if ( document.add_customer.state.value == "" )
+	    else if (document.add_customer.state.value == "" )
     {
         alert ( "Please enter your 'State's Name'." );
 		add_customer.state.focus();
         valid = false;
     }
-	    else if ( document.add_customer.nation.value == "" )
+	    else if (document.add_customer.nation.value == "" )
     {
         alert ( "Please enter your 'Nation's Name'." );
 		add_customer.nation.focus();
         valid = false;
     }
-	    else if ( document.add_customer.dob.value == "" )
+	    else if (document.add_customer.dob.value == "" )
     {
         alert ( "Please enter your 'Date of Birth'." );
 		add_customer.dob.focus();
         valid = false;
     }
-	    else if ( document.add_customer.gender.value == "" )
+	    else if (document.add_customer.gender.value == "" )
     {
         alert ( "Please select your 'Gender'." );
 		add_customer.gender.focus();
         valid = false;
     }
-	    else if ( document.add_customer.contact_no.value == "" )
+	    else if (document.add_customer.contact_no.value == "" )
     {
         alert ( "Please enter your 'Contact Number'." );
 		add_customer.contact_no.focus();
         valid = false;
     }
-	    else if ( document.add_customer.email_id.value == "" )
+	    else if (document.add_customer.email_id.value == "" )
     {
         alert ( "Please enter your 'Email ID'." );
 		add_customer.email_id.focus();
         valid = false;
     }
-	    else if ( document.add_customer.id_proof.value == "" )
+	    else if (document.add_customer.id_proof.value == "" )
     {
         alert ( "Please Select 'ID Proof'." );
 		add_customer.id_proof.focus();
